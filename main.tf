7f7b12df30289d3cb1169f65743cce7cc4b08062
Prajwal J A <80824448+Prajwal-JA@users.noreply.github.com>
2022-03-29 13:17:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 provider "azurerm" {
   version = ">=2.6"
+  
+  subscription_id = var.subscriptionID
+  client_id = var.clientID
+  client_secret = var.clientsecret
+  tenant_id = var.tenantID
 
 
   features {}
