b5c65adc531be19e013f92803a3848839cd999c7
Prajwal J A <80824448+Prajwal-JA@users.noreply.github.com>
2022-03-29 13:32:41 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -8,29 +8,6 @@ variable "location" {
     type        =   string
 }
 
-# Vnet Variables
-
-variable "virtual_network_name" {
-    description =   "Name of the virtual network"
-    type        =   string
-}
-
-variable "vnet_address_range" {
-    description =   "IP Range of the virtual network"
-    type        =   string
-}
-
-# Subnet Variables
-
-variable "subnet_name" {
-    description =   "Name of the subnet"
-    type        =   string
-}
-
-variable "subnet_address_range" {
-    description =   "IP Range of the virtual network"
-    type        =   string
-}
 
 variable "subscriptionID" {
     type = string
